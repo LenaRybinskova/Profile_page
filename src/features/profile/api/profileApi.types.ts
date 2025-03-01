669a1f4e2540703a765b465ae60b7554af89c7b1
«Elena <LenaRybinskova@gmail.com>
2025-03-01 21:19:22 +0300

    SCRUM-4 add display author and quote

--- src/features/profile/api/profileApi.types.ts
+++ src/features/profile/api/profileApi.types.ts
@@ -2,3 +2,8 @@ export type Profile = {
   fullname: string
   email: string
 }
+
+export type AuthorAndQuote = {
+  authorName: string
+  quote: string
+}
--- src/features/quote/api/quoteApi.ts
+++ src/features/quote/api/quoteApi.ts
@@ -1,7 +1,10 @@
-import type { GetQuote } from "@/features/quote/api/quoteApi.types"
+import { Author, Quote, QuoteArgs } from "../api/quoteApi.types"
+import { BaseResponse } from "../../auth/api/authApi.types"
 
+
+// TODO: reject протипизирвать
 export const quoteApi = {
-  getQuote: (data: GetQuote) => {
+  getQuote: (data: QuoteArgs):Promise<BaseResponse<Quote>> => {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         if (data.token === "fb566635a66295da0c8ad3f467c32dcf") {
@@ -10,13 +13,32 @@ export const quoteApi = {
             data: {
               quoteId: 1,
               authorId: 1,
-              quote: "A day without laughter is a day wasted.",
-            },
-          });
+              quote: "A day without laughter is a day wasted."
+            }
+          })
         } else {
-          reject(new Error("Invalid token"));
+          reject(new Error("Invalid token"))
         }
-      }, 5000);
-    });
+      }, 5000)
+    })
+  },
+
+  getAuthor: (token: string):Promise<BaseResponse<Author>> => {
+    return new Promise((resolve,reject) => {
+      setTimeout(() => {
+        if (token === "fb566635a66295da0c8ad3f467c32dcf") {
+          resolve({
+            success: true,
+            data: {
+              authorId: 1,
+              name: "Charlie Chaplin"
+            }
+          })
+        }
+        else{
+          reject(new Error("Invalid token"))
+        }
+      }, 5000)
+    })
   }
 }
